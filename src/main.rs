bac68a26ac393f1e87d44153f4e408f76cbc8da9
HUG0BA <bar23556@uvg.edu.gt>
2025-08-29 18:50:17 -0600

    Raytracer with basic intersect and material

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 
 mod conway;
 mod framebuffer;
+mod raytracer;
 
 use rand::Rng;
 use raylib::prelude::*;
@@ -9,19 +10,15 @@ use raylib::prelude::*;
 use std::thread;
 use std::time::Duration;
 
-use conway::Cell;
-use conway::Matrix;
 use framebuffer::Framebuffer;
 
-const MATRIX_WIDTH: usize = WINDOW_WIDTH as usize;
-const MATRIX_HEIGHT: usize = WINDOW_HEIGHT as usize;
+use raytracer::{Material, Sphere, render};
 
 const WINDOW_WIDTH: i32 = 1900;
 const WINDOW_HEIGHT: i32 = 1000;
 
 const FREMEBUFFER_WIDTH: i32 = WINDOW_WIDTH;
 const FRAMEBUFFER_HEIGHT: i32 = WINDOW_HEIGHT;
-const MATRIX_CELL_SCALLING_FACTOR: usize = 1;
 
 fn main() {
     game_loop();
@@ -37,70 +34,18 @@ fn game_loop() {
 
     let mut framebuffer = Framebuffer::new(FREMEBUFFER_WIDTH, FRAMEBUFFER_HEIGHT, Color::WHITE);
 
-    let mut game_of_life = Matrix::new(MATRIX_WIDTH, MATRIX_HEIGHT);
+    framebuffer.set_background_color(Color::new(80, 80, 200, 255));
 
-    //Initial values
-    //for i in 20..=30 {
-    //    game_of_life.set_cell(i, 25, Cell::Alive);
-    //}
-    //
-    let mut rng = rand::rng();
-
-    for i in 0..MATRIX_WIDTH {
-        for j in 0..MATRIX_HEIGHT {
-            let random_number = rng.random_range(0..100);
-            game_of_life.set_cell(
-                i,
-                j,
-                if random_number < 25 {
-                    Cell::Dead
-                } else {
-                    Cell::Alive
-                },
-            );
-        }
-    }
+    let objects = [Sphere::new(
+        Vector3::new(0.0, 0.0, -5.0),
+        1.0,
+        Material::new(Color::BLUEVIOLET),
+    )];
 
     while !&handle.window_should_close() {
-        let mut current_cell: Cell;
-        let mut current_color: Color;
+        framebuffer.clear();
 
-        let mut fx: usize;
-        let mut fy: usize;
-
-        for i in 0..MATRIX_WIDTH {
-            for j in 0..MATRIX_HEIGHT {
-                current_cell = game_of_life.get_cell(i, j);
-
-                current_color = match current_cell {
-                    Cell::Alive => Color::GOLD,
-                    Cell::Dead => Color::DARKBLUE,
-                };
-
-                framebuffer.set_foreground_color(current_color);
-
-                fx = i * MATRIX_CELL_SCALLING_FACTOR;
-                fy = j * MATRIX_CELL_SCALLING_FACTOR;
-
-                for px in fx..fx + MATRIX_CELL_SCALLING_FACTOR {
-                    for py in fy..fy + MATRIX_CELL_SCALLING_FACTOR {
-                        framebuffer.set_pixel(
-                            (framebuffer.width()
-                                - MATRIX_WIDTH as i32 * MATRIX_CELL_SCALLING_FACTOR as i32)
-                                / 2
-                                + px as i32,
-                            (framebuffer.height()
-                                - MATRIX_HEIGHT as i32 * MATRIX_CELL_SCALLING_FACTOR as i32)
-                                / 2
-                                + py as i32,
-                        );
-                    }
-                }
-            }
-        }
-
-        //let mouse_position: raylib::prelude::Vector2 = handle.get_mouse_position();
-        //let rectangle = Rectangle::new(85.0, 70.0, 250.0, 100.0);
+        render(&mut framebuffer, &objects);
 
         let texture = handle
             .load_texture_from_image(&raylib_thread, &framebuffer.color_buffer)
@@ -108,31 +53,9 @@ fn game_loop() {
 
         let mut draw_handle = handle.begin_drawing(&raylib_thread);
         {
-            //draw_handle.clear_background(Color::WHITE);
-
-            draw_handle.draw_texture(&texture, 0, 0, Color::LIGHTGRAY);
-
-            //draw_handle.draw_circle_v(mouse_position, 40.0, Color::INDIANRED);
-
-            //draw_handle.gui_button(rectangle, "Hello Word");
+            draw_handle.draw_texture(&texture, 0, 0, Color::WHITE);
         }
 
-        game_of_life = game_of_life.calculate_next_generation();
-
-        thread::sleep(Duration::from_millis(125));
+        //        thread::sleep(Duration::from_millis(125));
     }
 }
-
-/*fn test_matrix() {
-    let mut m = Matrix::new(MATRIX_WIDTH, MATRIX_HEIGHT);
-
-    m.set(0, 0, Cell::Alive);
-    m.set(0, 1, Cell::Alive);
-    m.set(1, 0, Cell::Alive);
-
-    println!("{m}");
-
-    m = m.calculate_next_generation();
-
-    println!("{m}");
-}*/
---
+++ src/raytracer.rs
@@ -0,0 +1,112 @@
+use std::mem::zeroed;
+
+use crate::{Framebuffer, framebuffer};
+use raylib::prelude::*;
+
+pub trait RayIntersect {
+    fn ray_intersect(&self, ray_origin: &Vector3, ray_direction: &Vector3) -> Intersect;
+}
+
+pub struct Sphere {
+    center: Vector3,
+    radius: f32,
+    material: Material,
+}
+
+impl Sphere {
+    pub fn new(center: Vector3, radius: f32, material: Material) -> Sphere {
+        Sphere {
+            center,
+            radius,
+            material,
+        }
+    }
+
+    pub fn center(&self) -> Vector3 {
+        self.center
+    }
+
+    pub fn radius(&self) -> f32 {
+        self.radius
+    }
+}
+
+impl RayIntersect for Sphere {
+    fn ray_intersect(&self, ray_origin: &Vector3, ray_direction: &Vector3) -> Intersect {
+        let oc = *ray_origin - self.center;
+
+        let a = ray_direction.dot(*ray_direction);
+
+        let b = 2.0 * oc.dot(*ray_direction);
+
+        let c = oc.dot(oc) - self.radius * self.radius;
+
+        let discriminant = b * b - 4.0 * a * c;
+
+        let is_intersecting = discriminant > 0.0;
+
+        Intersect::new(self.material, is_intersecting)
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct Material {
+    pub diffuse_color: Color,
+}
+
+impl Material {
+    pub fn new(diffuse_color: Color) -> Self {
+        Material { diffuse_color }
+    }
+
+    pub fn diffuse_color(&self) -> Color {
+        self.diffuse_color
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct Intersect {
+    pub material: Material,
+    pub is_intersecting: bool,
+}
+
+impl Intersect {
+    pub fn new(material: Material, is_intersecting: bool) -> Self {
+        Intersect {
+            is_intersecting,
+            material,
+        }
+    }
+}
+
+pub fn render(framebuffer: &mut Framebuffer, objects: &[Sphere]) {
+    let width = framebuffer.width() as f32;
+    let height = framebuffer.height() as f32;
+    let aspect_ratio = width / height;
+
+    for y in 0..framebuffer.height() {
+        for x in 0..framebuffer.width() {
+            let screen_x = (2.0 * x as f32) / width - 1.0;
+            let screen_y = -(2.0 * y as f32) / height + 1.0;
+
+            let screen_x = screen_x * aspect_ratio;
+
+            let ray_direction = Vector3::new(screen_x, screen_y, -1.0).normalized();
+
+            let pixel_color = cast_ray(&Vector3::zero(), &ray_direction, objects);
+
+            framebuffer.set_foreground_color(pixel_color);
+            framebuffer.set_pixel(x, y);
+        }
+    }
+}
+
+pub fn cast_ray(ray_origin: &Vector3, ray_direction: &Vector3, objects: &[Sphere]) -> Color {
+    for object in objects {
+        let intersection = object.ray_intersect(ray_origin, ray_direction);
+        if intersection.is_intersecting {
+            return Color::new(157, 165, 189, 255);
+        }
+    }
+    Color::new(4, 12, 36, 255)
+}
